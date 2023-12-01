5796e5b49ec8d6dc15310a3844c122e17d0926e6
Trung Dang <dangtrung96@gmail.com>
2023-12-01 14:57:22 +0700

    Minor.

--- algoapi/__init__.py
+++ algoapi/__init__.py
@@ -1,5 +1,5 @@
 class AlgoAPI:
 
-    def __init__(self, base_url='http://10.21.186.94:80') -> None:
+    def __init__(self, base_url: str = 'http://10.21.186.94:80') -> None:
         super().__init__()
         self.base_url = base_url
