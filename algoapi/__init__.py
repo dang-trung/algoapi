1f3ac7b389855183077b40ca5e9b9c41f242c00e
Trung Dang <dangtrung96@gmail.com>
2023-12-22 16:35:01 +0700

    Add Client verbose functionality.

--- algoapi/__init__.py
+++ algoapi/__init__.py
@@ -3,6 +3,11 @@ from algoapi.filter import FilterClient
 
 class AlgoAPI(FilterClient):
 
-    def __init__(self, base_url: str = 'http://10.21.186.94:80') -> None:
+    def __init__(
+        self,
+        base_url: str = 'http://10.21.186.94:80',
+        verbose: bool = False,
+    ) -> None:
         super().__init__()
         self.base_url = base_url
+        self.verbose = verbose
